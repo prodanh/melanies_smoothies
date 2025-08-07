5adfe636370d88af0f4a6e06cf3ada0d9d7e3404
prodanh <prodanh@ccf.org>
2025-08-07 14:21:49 -0400

    Update streamlit_app.py
    moved cnx= and session= back to lines 2-3

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,6 @@
 # Import python packages
+cnx = st.connection(“snowflake”)
+session = cnx.session()
 import streamlit as st
 from snowflake.snowpark.functions import col
 
@@ -11,8 +13,6 @@ st.write(
 name_on_order = st.text_input('Name on Smoothie')
 st.write('The name on your smoothie will be', name_on_order)
 
-cnx = st.connection(“snowflake”)
-session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
