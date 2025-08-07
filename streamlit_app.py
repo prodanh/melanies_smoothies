cb769f09c95d4e60418627d39e59f59cb0285c73
prodanh <prodanh@ccf.org>
2025-08-07 14:30:23 -0400

    Update streamlit_app.py
    back to internet code?????

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,6 @@
 # Import python packages
+cnx = st.connection ("snowflake")
+session = cnx.session()
 import streamlit as st
 from snowflake.snowpark.context import get_active_session
 #import get_active_session
@@ -12,9 +14,6 @@ st.write(
 
 name_on_order = st.text_input('Name on Smoothie')
 st.write('The name on your smoothie will be', name_on_order)
-
-#def get_active_session():
-session = get_active_session()
 
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
