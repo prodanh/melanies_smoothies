7dc1e4ee8460ae6f4d11d5f8ee9dc48c105a0e88
prodanh <prodanh@ccf.org>
2025-08-08 21:40:46 -0400

    Update streamlit_app.py
    added .json()) to line 50

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,6 +47,6 @@ if ingredients_list:
 # new section to display the smoothiefroot nutrition information
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
+st.text(smoothiefroot_response).json())
     
       
